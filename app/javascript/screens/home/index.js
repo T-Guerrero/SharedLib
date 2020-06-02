deb84b9e8710210e3cb396caa3699847b7f012d3
Thiago Benitez Pena <thiago.pena@usp.br>
2020-06-02 01:06:01 -0300

    Adicionado rota para DiscoveryScreen

---
+++ app/javascript/screens/home/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import HomeHeader from '../../components/home/HomeHeader';
+
+const HomeScreen = () => {
+    return (
+        <Fragment>
+            <HomeHeader />
+        </Fragment>
+    )
+}
+
+export default HomeScreen;
