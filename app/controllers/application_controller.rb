06ae95d6dc085b0de5f9bf8ac560e1557f26aca6
Carolina Marques <carolina.senra@usp.br>
2020-06-15 18:31:55 -0300

    logar com email e nusp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!, unless: -> { request.env['PATH_INFO'] == '/' }
 
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
 	def after_sign_in_path_for(resource) # método do devise
     '/books'
   end
+
+    def configure_permitted_parameters
+      added_attrs = [:nusp, :email, :password, :password_confirmation, :remember_me]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,24 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {minimum:3}, format: { with: /\A[[:alpha:] ]+\z/, message: "only allows letters" }
   validates :phone, presence: true, uniqueness: true, length: {minimum:11}, format: { with: /\A\(?[0-9]{2}\)? ?[0-9]+-?[0-9]+\z/, message: "special character not allowed" }
+  validates :nusp, presence: true, uniqueness: { case_sensitive: false }
+
+  devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :trackable, 
+      :validatable, authentication_keys: [:login]
+  attr_writer :login
+
+  def login
+    @login || self.nusp || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(nusp) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:nusp) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+  end
+end
 
 end
