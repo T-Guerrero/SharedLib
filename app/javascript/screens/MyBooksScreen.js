2f4bdc3b2760fcc50d90173271d47d98c831fcdb
Thiago Benitez Pena <thiago.pena@usp.br>
2020-08-03 23:28:59 -0300

    Adicionados CTAs em texto inicial em Meus Livros e Meus Empréstimos

--- app/javascript/screens/MyBooksScreen.js
+++ app/javascript/screens/MyBooksScreen.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { Columns, Heading } from 'react-bulma-components';
+import { Columns, Heading, Button } from 'react-bulma-components';
 import NavBar from '../components/navbar';
 import Book from '../components/book_preview';
 import { BookService } from '../services/index';
@@ -21,32 +21,51 @@ const MyBooksScreen = () => {
     const my_books_components = my_books.map((book, key) =>
         <Columns.Column key={key} className="is-one-third-desktop is-four-fifths-mobile">
             <Book
-            name={book.name} 
-            author={book.author} 
-            borrowed={book.borrowed} 
+            name={book.name}
+            author={book.author}
+            borrowed={book.borrowed}
             available={book.available}
             inTrasition={book.inTrasition}
-            image_url={book.image_url} 
-            key={key} 
+            image_url={book.image_url}
+            key={key}
             id={book.id}/>
         </Columns.Column>
     );
+
+    const bookCounter = my_books.length;
+
+
+    const buttonStyle = {
+        whiteSpace: "normal",
+        width: 160,
+        height: 60,
+        fontSize: 14,
+        borderRadius: 16
+    }
 
     return(
         <Fragment>
             <NavBar />
             <div style={{marginTop: 40, paddingLeft: 40, paddingRight: 40}}>
-                <Columns>
+                <Columns className="is-mobile">
                     <Columns.Column>
                         <Heading className='has-text-black' size={4}>
                         	Meus Livros
                         </Heading>
+                    </Columns.Column>
+                    <Columns.Column className="is-one-fifth-desktop">
+                      <a href="/books/new">
+                          <Button className="is-info" style={buttonStyle}>Adicione um novo livro</Button>
+                      </a>
                     </Columns.Column>
                 </Columns>
                 <Columns className='is-mobile center2-mobile'>
                     {my_books_components}
         		</Columns>
             </div>
+            {bookCounter == 0 &&
+                    <p>Você ainda não adicionou nenhum livro. Adicione um para poder pegar livros emprestados.</p>
+            }
         </Fragment>
     );
 }
--- app/javascript/screens/MyBorrowingsScreen.js
+++ app/javascript/screens/MyBorrowingsScreen.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import Pluralize from 'pluralize';
-import { Columns, Heading } from 'react-bulma-components';
+import { Columns, Heading, Button } from 'react-bulma-components';
 import NavBar from '../components/navbar';
 import { DashboardService } from '../services/index';
 import Book from '../components/book_preview';
@@ -24,6 +24,14 @@ const MyBorrowingsScreen = () => {
         return `${d.getDate()}/${1 + d.getMonth()}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
     }
 
+    const buttonStyle = {
+        whiteSpace: "normal",
+        width: 160,
+        height: 60,
+        fontSize: 14,
+        borderRadius: 16
+    }
+
     const my_borrowings_components = borrowings.map((borrowing, key) =>
             <Columns.Column key={key} className="is-one-third-desktop is-four-fifths-mobile">
                 <Book name={borrowing.book.name} author={borrowing.book.author} borrowed={borrowing.book.borrowed} available={borrowing.book.available} image_url={borrowing.book.image_url} key={key} id={borrowing.book.id}/>
@@ -31,11 +39,13 @@ const MyBorrowingsScreen = () => {
             </Columns.Column>
     );
 
+    const borrowingsCounter = borrowings.length;
+
     return(
         <Fragment>
             <NavBar />
             <div style={{marginTop: 40, paddingLeft: 40, paddingRight: 40}}>
-                <Columns className="is-vcentered">
+                <Columns className="is-vcentered is-mobile">
                     <Columns.Column>
                         <Heading className='has-text-black' size={4}>
                         Meus Empréstimos
@@ -46,10 +56,19 @@ const MyBorrowingsScreen = () => {
                         Você pode pegar {Pluralize("livro", user.maxBorrowings, true)} {Pluralize("emprestado", user.maxBorrowings)}
                         </Heading>
                     </Columns.Column>
+                    <Columns.Column className="is-one-fifth-desktop">
+                      <a href="/books">
+                          <Button className="is-info" style={buttonStyle}>Pegue um novo livro emprestado</Button>
+                      </a>
+                    </Columns.Column>
                 </Columns>
                 <Columns className='is-mobile center2-mobile'>
                     {my_borrowings_components}
                 </Columns>
+                {borrowingsCounter == 0 &&
+                        <p>Você ainda não pegou nenhum livro emprestado.</p>
+                }
+
             </div>
         </Fragment>
     );
